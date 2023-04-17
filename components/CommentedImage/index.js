92ffa37b0c12c4db91d30d07b74c4943cf40b69b
Aidan <datbluemushroom@gmail.com>
2023-04-16 21:17:23 -0400

    🩹 CommentedImage Touch-up

--- components/CommentedImage/index.js
+++ components/CommentedImage/index.js
@@ -8,7 +8,7 @@ const CommentedImage = ({ src, text }) =>
     return (
         <>
         
-            <div className="flex flex-col items-center bg-pink-100 transition-all duration-300 hover:shadow-lg hover:bg-pink-200">
+            <div className="flex flex-col items-center bg-pink-100 transition-all duration-300 hover:shadow-lg hover:bg-pink-200 cursor-pointer">
                 <Image className="p-5" src={src} width="250" height="200" onMouseOver={onToggle} onMouseOut={onToggle} />
                 <Collapse in={isOpen}>
                     <h1 className="text-pink-400 font-bold uppercase pb-2">{text}</h1>

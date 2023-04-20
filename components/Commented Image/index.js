456471c6018e2d3754e0f6316e337229b037d67f
Aidan <datbluemushroom@gmail.com>
2023-04-20 02:39:44 -0400

    🐛 More Alts

--- components/Commented Image/index.js
+++ components/Commented Image/index.js
@@ -9,7 +9,7 @@ const CommentedImage = ({ src, text }) =>
         <>
         
             <div className="flex flex-col items-center bg-pink-100 transition-all duration-300 hover:shadow-lg hover:bg-pink-200 cursor-pointer">
-                <Image className="p-5" src={src} width="250" height="200" onMouseOver={onToggle} onMouseOut={onToggle} />
+                <Image className="p-5" src={src} width="250" height="200" onMouseOver={onToggle} onMouseOut={onToggle} alt=''/>
                 <Collapse in={isOpen}>
                     <h1 className="text-pink-400 font-bold uppercase pb-2">{text}</h1>
                 </Collapse>
